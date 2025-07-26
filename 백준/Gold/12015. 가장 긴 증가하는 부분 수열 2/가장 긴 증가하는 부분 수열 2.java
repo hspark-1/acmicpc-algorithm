46405d755349062ee6154574fae71aa09471c10f
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2025-07-27 00:45:56 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 580 ms, Memory: 164764 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -4,37 +4,6 @@ import java.io.*;
 public class Main {
     public static int N;
     public static int[] arr;
-
-    public static int lowerBound(int[] arr, int value, int right) {
-        int left = 0;
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-
-            if (arr[mid] < value) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        return left;
-    }
-
-    public static boolean check(int mid) {
-        int[] note = new int[N+1];
-        note[0] = arr[0];
-        int endIdx = 0;
-        for (int i=1; i<N; i++) {
-            if (note[endIdx] < arr[i]) {
-                note[++endIdx] = arr[i];
-            }
-            else {
-                note[lowerBound(note, arr[i], endIdx)] = arr[i];
-            }
-        }
-        return endIdx + 1 >= mid;
-    }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,17 +12,16 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        int left = 1;
-        int right = N;
-        int answer = 0;
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(arr[0]);
+        for (int i=1; i<N; i++) {
+            int pos = Collections.binarySearch(arrayList, arr[i]);
+            if (pos < 0) pos = - (pos + 1);
 
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            if (check(mid)) {
-                answer = mid;
-                left = mid + 1;
-            } else right = mid - 1;
+            if (pos == arrayList.size()) arrayList.add(arr[i]);
+            else arrayList.set(pos, arr[i]);
         }
-        System.out.print(answer);
+
+        System.out.print(arrayList.size());
     }
 }
